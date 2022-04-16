0bfa6c36b301ed8fa59cc4da1c39f7bc4bbe489e
Jake Starr <jakestarr60@gmail.com>
2022-04-16 16:11:48 -0400

    Init app

---
+++ BlogApp/Models/Base.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogApp.Models
+{
+    public class Base
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
