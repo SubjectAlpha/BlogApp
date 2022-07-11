0d37201e678262fa4ca933117e1e8455da0a7fd5
Jake Starr <jakestarr60@gmail.com>
2022-07-10 21:13:37 -0400

    Document account helper

--- Utility/AccountHelper.cs
+++ Utility/AccountHelper.cs
@@ -5,56 +5,58 @@ using System.Text;
 
 namespace BlogApp.Utility
 {
-    public struct EncryptedPair
-    {
-        public string Hash;
-        public string Salt;
-    }
-
     public class AccountHelper
     {
-        public const string CookieSeparationSequence = "!$%#!";
         private const string Lower = @"abcdefghijklmnopqrstuvwxyz";
         private const string Upper = @"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         private const string Digits = @"1234567890";
         private const string SpecialChars = @"~!@#$%^&*()-_=+[{]}\|;"":',<.>/?";
         private const int SaltSize = 16, HashSize = 24, HashIter = 10000;
 
+        //Create a struct to hold the values in a named pair
+        public struct EncryptedPair
+        {
+            public string Hash;
+            public string Salt;
+        }
+
+        //This method will securely generate a password for the defined character set with no modulo bias.
         public static string GeneratePassword(int requiredLength)
         {
             var source =
-                $"{Lower}{Upper}{Digits}{SpecialChars}".ToCharArray();
-            var crypto = new RSACryptoServiceProvider();
-            var result = new StringBuilder(requiredLength);
+                $"{Lower}{Upper}{Digits}{SpecialChars}".ToCharArray(); // Load all allowable characters into single array
+            var crypto = new RSACryptoServiceProvider(); //Instantiate the cryptographic service provider.
+            var result = new StringBuilder(requiredLength); //This will allow us to create a string easily through the iterations.
+            var emptyArr = Array.Empty<byte>(); //Instantiate outside of the loop for increased performance.
 
             while (result.Length < requiredLength)
             {
-                var data = crypto.Encrypt(new byte[1], false);
+                var data = crypto.Encrypt(emptyArr, false); //Generate cryptographically secure random
 
-                if (data[0] < (source.Length * 2))
+                if (data[0] < (source.Length * 2)) //Ensure that the random generated is at least twice as big as the source array. This is what combats modulo bias.
                 {
-                    var currentValue = data[0] % source.Length;
-                    result.Append(source[currentValue]);
+                    var currentValue = data[0] % source.Length; //Take the remainder of our character against the character array
+                    result.Append(source[currentValue]); //Choose a character from the source array and append it to the string builder.
                 }
             }
 
-            return result.ToString();
+            return result.ToString(); //Send the result of the string builder.
         }
 
         public static EncryptedPair EncryptPassword(string password)
         {
-            var salt = new RSACryptoServiceProvider().Encrypt(new byte[SaltSize], false);
-            var hash = new Rfc2898DeriveBytes(password, salt, HashIter).GetBytes(HashSize);
-            return new EncryptedPair { Hash = Convert.ToBase64String(hash), Salt = Convert.ToBase64String(salt) };
+            var salt = new RSACryptoServiceProvider().Encrypt(new byte[SaltSize], false); //Generate a new salt using the RSA algorithm.
+            var hash = new Rfc2898DeriveBytes(password, salt, HashIter).GetBytes(HashSize); //Salt and hash the password.
+            return new EncryptedPair { Hash = Convert.ToBase64String(hash), Salt = Convert.ToBase64String(salt) }; //Return the encrypted pair, the values have also been base64 encoded.
         }
         
         public static bool VerifyPassword(string password, EncryptedPair encryptionInfo)
         {
-            var testArray = new Rfc2898DeriveBytes(password, Convert.FromBase64String(encryptionInfo.Salt), HashIter).GetBytes(HashSize);
+            var testArray = new Rfc2898DeriveBytes(password, Convert.FromBase64String(encryptionInfo.Salt), HashIter).GetBytes(HashSize); //Unencode the stored hash, and then generate a new hashed password based on the password supplied.
             var unencodedHash = Convert.FromBase64String(encryptionInfo.Hash);
             for (int i = 0; i < HashSize; i++)
             {
-                if (testArray[i] != unencodedHash[i])
+                if (testArray[i] != unencodedHash[i]) //Loop through each character of the hashed password and ensure they match what was expected.
                     return false;
             }
             return true;
@@ -76,7 +78,7 @@ namespace BlogApp.Utility
 
         public static bool IsValidEmail(string email)
         {
-            var trimmedEmail = email.Trim();
+            var trimmedEmail = email.Trim(); //Remove whitespace
             if(trimmedEmail.EndsWith("."))
             {
                 return false;
@@ -84,8 +86,8 @@ namespace BlogApp.Utility
 
             try
             {
-                var mailAddress = new System.Net.Mail.MailAddress(email);
-                return mailAddress.Address == trimmedEmail;
+                var mailAddress = new System.Net.Mail.MailAddress(email); //This will throw an exception if the email address is invalid.
+                return mailAddress.Address == trimmedEmail; //Ensure the emails are the same.
             }
             catch (Exception)
             {
@@ -95,6 +97,7 @@ namespace BlogApp.Utility
 
         public static bool IsLoggedIn(HttpContext httpContext)
         {
+            //Check the session to see if the logged in string is set to true.
             var sessionLoggedIn = httpContext.Session.GetString("loggedIn");
             return (!string.IsNullOrEmpty(sessionLoggedIn) && string.Compare(sessionLoggedIn, "true") == 0);
         }
